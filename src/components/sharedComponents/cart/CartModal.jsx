59e33c90f24f09d420013489734090f8c836be86
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-21 21:00:06 -0500

    added chared styles for modal spacing, cart modal buttons

--- src/components/sharedComponents/cart/CartModal.jsx
+++ src/components/sharedComponents/cart/CartModal.jsx
@@ -9,12 +9,12 @@ from #App
 export default function CartModal() {
   return (
     <section className="cartModal">
-      <div className="cartModal__titleWrapper">
+      <div className="spacedItems cartModal__titleWrapper">
         <h2>Cart (num)</h2>
         <button className="cartModal__removeButton">Remove All</button>
       </div>
       <ItemContainer />
-      <div className="cartModal__totalWrapper">
+      <div className="spacedItems cartModal__totalWrapper">
         <h3>Total</h3>
         <p className="cartModal__totalAmount">$ 5,342</p>
       </div>
