e5da2d46aeb9f5d0b8b6e2b37b99ddf64dc7dad5
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-21 21:44:57 -0500

    added temporary cart for testing and building component

--- src/components/sharedComponents/cart/ItemContainer.jsx
+++ src/components/sharedComponents/cart/ItemContainer.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import ItemRow from "./ItemRow";
+
+const tempCart = [
+  {
+    id: 1,
+    image: "./assets/cart/image-xx99-mark-two-headphones.jpg",
+    name: "XX99 MK II",
+    price: 2999,
+  },
+  {
+    id: 2,
+    image: "./assets/cart/image-xx59-headphones.jpg",
+    name: "XX59",
+    price: 899,
+  },
+  {
+    id: 3,
+    image: "./assets/cart/image-yx1-earphones.jpg",
+    name: "YX1",
+    price: 599,
+  },
+];
 
 export default function ItemContainer() {
-  return <div>container</div>;
+  return tempCart.length ? (
+    <section>
+      {tempCart.map((item) => (
+        <ItemRow key={item.id} item={item} />
+      ))}
+    </section>
+  ) : (
+    "No item in the cart"
+  );
 }
